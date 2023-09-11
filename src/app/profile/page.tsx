5b92ca02045d18d0067e0d0535d8c6abfd58b1c7
Teshiification <panini62@web.de>
2023-09-11 15:48:40 +0200

    fix lint errors

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,23 +1,15 @@
-import { getUser } from '@/lib/database';
+import { redirect } from 'next/navigation';
 
 export const dynamic = 'force-dynamic';
 
 export default async function Index() {
-  const user = await getUser();
+  const user = null;
 
+  if (!user) redirect('login');
   return (
     <div className="flex flex-col w-screen h-screen items-center select-none overflow-y-auto">
       <div className="w-full h-full flex flex-col px-2">
-        {user ? (
-          <>
-            {
-              //@ts-ignore
-              <TicketCarousel tickets={tickets || []} />
-            }
-          </>
-        ) : (
-          <div>Entdecken Sie neue Reiseziele.</div>
-        )}
+        <div>Entdecken Sie neue Reiseziele.</div>
       </div>
     </div>
   );
