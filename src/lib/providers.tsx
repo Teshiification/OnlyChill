82639509eb2c5462e05fa1048a94e9ad27da80bd
Teshiification <panini62@web.de>
2023-10-11 23:04:48 +0200

    fix(typescript): typerror

--- src/lib/providers.tsx
+++ src/lib/providers.tsx
@@ -7,12 +7,12 @@ import { User } from 'next-auth';
 
 export const UserContext = createContext<User | null>(null);
 
-export async function UserProvider({ children }) {
+export async function UserProvider({ children }: any) {
   const supabase = createServerComponentClient({ cookies });
   const {
     data: { user }
   } = await supabase.auth.getUser();
   return (
-    <UserContext.Provider value={{ user }}>{children}</UserContext.Provider>
+    <UserContext.Provider value={user as User}>{children}</UserContext.Provider>
   );
 }
